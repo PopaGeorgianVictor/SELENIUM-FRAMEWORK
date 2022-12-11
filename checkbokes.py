e682c36c268e435b9a7185fd2eeb4491673331a8
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-11 14:57:51 +0200

    framework

--- checkbokes.py
+++ checkbokes.py
@@ -7,10 +7,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
 
 class Checkbox(unittest.TestCase):
-    to_select_value = '21-40'
-    locator_by_value = f'input[name="age-group-checkbox"][value="{to_select_value}"]'
-    my_choice = (By.CSS_SELECTOR, locator_by_value)
-    all_checkboxes = (By.NAME, 'age-group-checkbox')
+    TO_SELECT_VALUE = '21-40'
+    LOCATOR_BY_VALUE = f'input[name="age-group-checkbox"][value="{TO_SELECT_VALUE}"]'
+    MY_CHOICE = (By.CSS_SELECTOR, LOCATOR_BY_VALUE)
+    ALL_CHECKBOXES = (By.NAME, 'age-group-checkbox')
 
     def setUp(self) -> None:
         self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
@@ -22,13 +22,13 @@ class Checkbox(unittest.TestCase):
         self.driver.quit()
 
     def test_select_one_checkbox(self):
-        selected_ch =  self.driver.find_element(*self.my_choice)
+        selected_ch =  self.driver.find_element(*self.MY_CHOICE)
         selected_ch.click()
-        assert selected_ch.is_selected(), f"After clicking value {to_select_value} it is not selected."
+        assert selected_ch.is_selected(), f"After clicking value {TO_SELECT_VALUE} it is not selected."
 
     def test_select_all_checkbokes(self):
         expected_number_of_options = 4
-        all_ch = self.driver.find_elements(*self.all_checkboxes)
+        all_ch = self.driver.find_elements(*self.ALL_CHECKBOXES)
         assert len(all_ch) == expected_number_of_options, "Number of checkboxes is not the expected"
         for checkbox in all_ch:
             checkbox.click()
