cb58cea9cb890b83f3027d5d1ba5cead6dca31cf
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-13 15:00:45 +0200

    test site

--- reports/auth.py
+++ reports/auth.py
@@ -8,16 +8,14 @@ import HTMLTestRunner
 
 class Keyboard(unittest.TestCase):
 
-    USERNAME_LG = (By.ID, "logName")
-    PASSWORD_LG = (By.ID, "logPassword")
-    USERNAME_SG = (By.ID, "signName")
-    PASSWORD_SG = (By.ID, "signPassword")
-    EMAIL_SG = (By.ID, "signEmail")
+    USERNAME = (By.ID, "signName")
+    PASSWORD = (By.ID, "signPassword")
+    EMAIL = (By.ID, "signEmail")
 
     def setUp(self) -> None:
         self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
         self.driver.maximize_window()
-        self.driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/login_sign_up")
+        self.driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/login_sign_up#signup")
         self.driver.implicitly_wait(2)
 
     def tearDown(self) -> None:
