c296a7fb6124a9a1e9647f4398e730b4d74db95c
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-19 14:49:23 +0200

    framework

---
+++ sliders.py
@@ -0,0 +1,23 @@
+import unittest
+import HTMLTestRunner
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver import ActionChains
+
+class Sliders(unittest.TestCase):
+
+    CLICK = (By.CSS_SELECTOR, "#contextMenu a")
+
+
+    def setUp(self) -> None:
+        self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        self.driver.maximize_window()
+        self.driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/right_click_menu")
+        self.driver.implicitly_wait(2)
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+    def test_right_click(self):
