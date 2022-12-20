e85fac77e60cb42bd05b0cde1c784ad1285043ab
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-20 15:22:34 +0200

    way 2 automation

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver import ActionChains
+
 
 class Sliders(unittest.TestCase):
 
