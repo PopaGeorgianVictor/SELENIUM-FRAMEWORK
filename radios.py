7ee8ce4cce3023fc2589c52e673c5640fbd355d3
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 20:29:44 +0200

    framework

--- radios.py
+++ radios.py
@@ -5,11 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
-
+from selenium.common.exceptions import WebDriverException
 
 class Radios(unittest.TestCase):
     LOCATOR_BY_VALUE = 'input[name="radio-stations"][value="{value}"]'
     RADIOS = (By.NAME, 'radio-stations')
+    BTN1 = (By.CSS_SELECTOR, "input[value='magic fm']")
+    BTN2 = (By.CSS_SELECTOR, "input[value='radio galaxy']")
 
     def setUp(self) -> None:
         self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
@@ -26,11 +28,18 @@ class Radios(unittest.TestCase):
         default_element = self.driver.find_element(By.CSS_SELECTOR, self.LOCATOR_BY_VALUE.format(value=expected_default_value))
         assert default_element.is_selected(), f"The default value of {expected_default_value} is not selected."
 
-    def test_click_all_btn(self):
-        self.driver.find_element(By.CSS_SELECTOR, "input[value='magic fm']").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input[value='radio galaxy']").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input[value='europa fm']").click()
-        self.driver.find_element(By.CSS_SELECTOR, "input[value='rock fm']").click()
+    def test_elem_clickable(self):
+        try:
+            self.driver.find_element(*self.BTN1).click()
+            print("Magic FM button is clickable")
+        except WebDriverException:
+            print("Magic FM button is not clickable")
+
+        try:
+            self.driver.find_element(*self.BTN2).click()
+            print("Radio Galaxy button is clickable")
+        except WebDriverException:
+            print("Radio Galaxy button is not clickable")
 
     def test_verify_number_of_radio_btn(self):
         expected_values = ['magic fm','radio galaxy','europa fm','rock fm']
