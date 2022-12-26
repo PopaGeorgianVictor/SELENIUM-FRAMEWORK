db0ad511ed8f431974ed8136e77593d86ab6a0a8
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 10:33:36 +0200

    framework

--- right_click.py
+++ right_click.py
@@ -1,5 +1,7 @@
+import time
 import unittest
 import HTMLTestRunner
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
@@ -9,7 +11,7 @@ from selenium.webdriver import ActionChains
 class Right_Click(unittest.TestCase):
 
     CLICK = (By.CSS_SELECTOR, "#contextMenu a")
-
+    ELEM =(By.LINK_TEXT,'PORTOFOLIO')
 
     def setUp(self) -> None:
         self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
@@ -24,6 +26,16 @@ class Right_Click(unittest.TestCase):
         ActionChains(self.driver).context_click().perform()
         self.driver.find_element(*self.CLICK).click()
         print('I clicked on OVERVIEW')
+        time.sleep(3)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        print("Second window title = " + self.driver.title)
+
+        try:
+            self.driver.find_element(*self.ELEM)
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
 
 if __name__ == '__main__' :
     unittest.main(testRunner=HTMLTestRunner.HTMLTestRunner(output='D:/selenium project/framework/reports'))
