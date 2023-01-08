0141552c0644de01881bf5cb3b3ac71e755f47fa
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-08 19:17:03 +0200

    bdd-pom

--- demostore.py
+++ demostore.py
@@ -14,3 +14,11 @@ class Login(unittest.TestCase):
     ERROR_TEXT = (By.XPATH,"//div[@id='content']//li[1]")
     PASSWORD_HINT = (By.CSS_SELECTOR, ".woocommerce-password-hint")
 
+    def setUp(self) -> None:
+        self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        self.driver.maximize_window()
+        self.driver.get("http://demostore.supersqa.com/my-account/")
+        self.driver.implicitly_wait(2)
+
+    def test_insert_email(self):
+        self.driver.find_element(*self.EMAIL).send_keys("test123@gmail.com")
